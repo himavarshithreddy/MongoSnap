c0dec67c02ed7fb012563388052fbc0d899e34e6
Himavarshith Reddy <himavarshithreddy@gmail.com>
2025-06-22 18:51:54 +0530

    a

--- node_modules/.pnpm/node_modules/frontend/vite.config.js
+++ node_modules/.pnpm/node_modules/frontend/vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { qrcode } from 'vite-plugin-qrcode' // ✅ FIX: named import
+import { qrcode } from 'vite-plugin-qrcode' 
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    qrcode() // ✅ Use the named function
+    qrcode(),
+    tailwindcss()
   ],
   server: {
     host: true,
     proxy: {
-      '/api': 'http://192.168.1.10:4000', // 👈 replace with your actual local IP
+      '/api': 'http://192.168.1.10:4000', // 
     }
   }
 })
